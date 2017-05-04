6302998559c886da46546fd8482d3ceb362a1025
tjking20 <tjking20@outlook.com>
2017-05-04 11:42:48 -0400

    app can pull random joke from database, and display dynamically in expo

--- client/App.js
+++ client/App.js
@@ -1,13 +1,150 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
+import { Constants } from 'expo';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor() {
+  super();
+  // const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+  this.state = {
+    joke: "",
+    newJoke: ""
+  };
+}
+
+  componentDidMount(){
+    fetch("https://dad-o-myte.herokuapp.com/jokes")
+    .then(res => res.json())
+    .then(jokes => {
+
+
+      let joke = jokes[Math.floor(Math.random()*jokes.length)]
+      console.log(joke)
+      this.setState({joke: joke.joke})
+    })
+  }
+  
+  // _handleButtonPress = () => {
+  //   return fetch("https://morning-mesa-23625.herokuapp.com/songs", {
+  //     method: 'POST',
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({
+  //       artist: this.state.artist,
+  //       songName: this.state.song,
+  //       votes: 0
+  //     })
+  //   }).then(res => res.json())
+  //   .then(savedSong => {
+  //     let songsUpdated = [...this.state.songs._dataBlob.s1, savedSong];
+  //     this.setState({
+  //       songs : this.state.songs.cloneWithRows(songsUpdated)
+  //     })
+  //   })
+  // };
+
+  // _handleDelete = (_id) => {
+  //   console.log(_id)
+
+  //   return fetch(`https://morning-mesa-23625.herokuapp.com/songs/${_id}`, {
+  //     method: 'DELETE',
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json'
+  //     }
+  //   }).then(res => res.json())
+  //   .then(oldSongId => {
+  //     let songsUpdated = this.state.songs._dataBlob.s1.filter((song, i) => song._id !== oldSongId)
+  //     console.log('deleted id ', oldSongId)
+  //     this.setState({
+  //       songs : this.state.songs.cloneWithRows(songsUpdated)
+  //     });
+  //   })
+  // };
+  
+  // /*
+  // function _handleVote(_id, direction) {
+  
+  // }
+  // */
+  // _handleVote = (_id, direction) => {
+  //   console.log(_id, direction);
+    
+  //   return fetch(`https://morning-mesa-23625.herokuapp.com/songs/votes/${_id}/${direction}`, {
+  //     method: 'PUT',
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json'
+  //     }
+  //   }).then(res => res.json())
+  //   .then(editedSong => {
+  //     let songsUpdated = this.state.songs._dataBlob.s1.map(sng => {
+  //       return editedSong._id == sng._id ? editedSong : sng
+  //     });
+      
+  //     this.setState({
+  //       songs : this.state.songs.cloneWithRows(songsUpdated)
+  //     });
+  //   })
+  // }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Dad O Myte!</Text>
+        <Text>{this.state.joke}</Text>
+       
+
+      {/*  
+        <Text style={styles.heading}>Add a New Song</Text>
+        <TextInput
+          style={{ width: 200, height: 44, padding: 8 }}
+          defaultValue="artist"
+          onChangeText={(text) => this.setState({artist: text})}
+        />
+        
+        <TextInput
+          style={{ width: 200, height: 44, padding: 8 }}
+          defaultValue="song"
+          onChangeText={(text) => this.setState({song: text})}
+        />
+        
+        <Button
+          title="Add Song"
+          onPress={this._handleButtonPress}
+        />
+    
+      <ListView
+        dataSource={this.state.songs}
+        renderRow={(song) => 
+        <View style={styles.container}>
+        
+        <Text style={styles.item}>Artist: {song.artist} | Song: {song.songName} | Votes: {song.votes}</Text>
+        
+        <Button
+          title="Up Vote"
+          onPress={() => this._handleVote(song._id, 'up')}
+        />
+        
+        <Button
+          title="Down Vote"
+          onPress={() => this._handleVote(song._id, 'down')}
+        />
+        
+        <Button
+          title="Delete"
+          onPress={() => this._handleDelete(song._id)}
+        />
+
+        <Button
+          title="Edit"
+          onPress={() => this.handleEdit()}
+        />
+        
+        </View>}
+      />*/}  
       </View>
     );
   }
@@ -16,8 +153,16 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
   },
+  item: {
+    marginTop: 10
+  },
+  heading: {
+    fontSize: 28,
+    fontWeight: 'bold'
+  }
 });
